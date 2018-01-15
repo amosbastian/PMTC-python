979968e3c8bb38203a037f366c5c283d7967298c
amosbastian <amos_bastian@hotmail.com>
2018-01-15 18:14:22 +0000

    Scoreboard working, sort of

--- mhp.py
+++ mhp.py
@@ -4,19 +4,51 @@ import sys
 
 API_BASE_URL = "https://acs.leagueoflegends.com/v1/stats/game/TRLH3/{}"
 
+# /r/LeagueOfLegends
+VS = "[vs](#mt-kills)"
+GOLD = "[G](#mt-gold)"
+TOWERS = "[T](#mt-towers)"
+
 class Player(object):
-    def __init__(self, player_id, win, kills, deaths, assists, gold_earned):
-        self.id = player_id
-        self.win = win
-        self.kills = kills
-        self.deaths = deaths
+    """A player participating in a League of Legends match."""
+    def __init__(self, player_id, win, kills, deaths, assists, gold_earned,
+        champion):
+
         self.assists = assists
+        self.champion = champion
+        self.deaths = deaths
         self.gold_earned = gold_earned
+        self.id = player_id
+        self.kills = kills
+        self.position = self.get_position(self.id)
+        self.win = win
+
+    def get_position(self, player_id):
+        """ Gets the player's position depending on their player_id."""
+        if player_id == 1 or player_id == 6:
+            return "TOP"
+        elif player_id == 2 or player_id == 7:
+            return "JNG"
+        elif player_id == 3 or player_id == 8:
+            return "MID"
+        elif player_id == 4 or player_id == 9:
+            return "ADC"
+        else:
+            return "SUP"
 
 def create_player(obj):
-        return Player(obj["participantId"], obj["win"], obj["kills"],
-            obj["deaths"], obj["assists"], obj["goldEarned"])
+    """ Creates a Player object out of the given obj."""
+    stats = obj["stats"]
+    return Player(stats["participantId"], stats["win"], stats["kills"],
+        stats["deaths"], stats["assists"], stats["goldEarned"],
+        obj["championId"])
 
+def team_kda(players):
+    kills = sum([player.kills for player in players])
+    deaths = sum([player.deaths for player in players])
+    assists = sum([player.assists for player in players])
+
+    return "{}-{}-{}".format(kills, deaths, assists)
 
 def team_split(players):
     team_1 = players[0]["player"]["summonerName"].split()[0]
@@ -30,8 +62,18 @@ if __name__ == '__main__':
     game_duration = round(request["gameDuration"] / 60)
     teams = request["teams"]
 
-    player_objects = [create_player(player["stats"]) for player in players]
+    player_objects = [create_player(player) for player in players]
 
-    for player in player_objects:
-        print("{} {}-{}-{}".format(player.id, player.kills, player.deaths,
-            player.assists))
+    players_1 = player_objects[:5]
+    players_2 = player_objects[5:]
+
+    team_1_kda = team_kda(players_1)
+    team_2_kda = team_kda(players_2)
+
+    print("{} {} {}".format(team_1_kda, VS, team_2_kda))
+    for player_1, player_2 in zip(players_1, players_2):
+        print("{} {} {}-{}-{} {} {}-{}-{} {} {}".format(
+            player_1.id, player_1.champion, player_1.kills, player_1.deaths,
+            player_1.assists, player_1.position, player_2.kills,
+            player_2.deaths, player_2.assists, player_2.champion, player_2.id
+            ))
