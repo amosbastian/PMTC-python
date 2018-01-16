4ed03cd2780ec35c54e571104bc4bd302b1e536a
amosbastian <amos_bastian@hotmail.com>
2018-01-16 11:38:48 +0000

    Can now print scoreboard

--- mhp.py
+++ mhp.py
@@ -3,6 +3,7 @@ import requests
 import sys
 
 API_BASE_URL = "https://acs.leagueoflegends.com/v1/stats/game/TRLH3/{}"
+CHAMPION_URL = "http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json"
 
 # /r/LeagueOfLegends
 VS = "[vs](#mt-kills)"
@@ -38,6 +39,14 @@ class Player(object):
         else:
             return "SUP"
 
+def champion_dictionary(obj):
+    """Converts champion.json to a dictionary that is useful to us."""
+    champions = {}
+    for champion in obj["data"]:
+        name = champion.lower()
+        champions[int(obj["data"][champion]["key"])] = name
+    return champions
+
 def create_player(player_info, obj):
     """ Creates a Player object out of the given information and obj."""
     stats = obj["stats"]
@@ -55,13 +64,38 @@ def team_kda(players):
 
     return "{}-{}-{}".format(kills, deaths, assists)
 
+def champion_converter(champion_id):
+    champion_id = "{}".format(champion_id)
+    if champion_id in champions:
+        champion = champions[champion_id]
+    else:
+        champion = champion_id
+    return "[{}](#c-{})".format(champion, champion)
+
 def team_split(players):
     team_1 = players[0]["player"]["summonerName"].split()[0]
     team_2 = players[5]["player"]["summonerName"].split()[0]
     return team_1, team_2
 
+def print_scoreboard():
+    print("|**{}**|{}|{}|{}|**{}**|".format(team_1_short, team_1_kda, VS,
+        team_2_kda, team_2_short))
+    print("|--:|--:|:--:|:--|:--|")
+    for player_1, player_2 in zip(players_1, players_2):
+        print("|{} {}|{}-{}-{}|{}|{}-{}-{}|{} {}|".format(
+            player_1.name, champion_converter(player_1.champion),
+            player_1.kills, player_1.deaths, player_1.assists,
+            player_1.position, player_2.kills, player_2.deaths,
+            player_2.assists, champion_converter(player_2.champion),
+            player_2.name
+            ))
+
 if __name__ == '__main__':
-    match_history = sys.argv[1].split("/")[-1]
+    # match_history = sys.argv[1].split("/")[-1]
+    match_history = "1002320090?gameHash=a534cb383e6b49dc&tab=overview"
+    with open("champion.json", "r") as json_data:
+        champions = json.load(json_data)
+
     request = requests.get(API_BASE_URL.format(match_history)).json()
     players = request["participants"]
     game_duration = round(request["gameDuration"] / 60)
@@ -78,11 +112,4 @@ if __name__ == '__main__':
     team_2_kda = team_kda(players_2)
     team_2_short = players_2[0].team
 
-    print("{} {} {} {} {}".format(team_1_short, team_1_kda, VS, team_2_kda,
-        team_2_short))
-    for player_1, player_2 in zip(players_1, players_2):
-        print("{} {} {}-{}-{} {} {}-{}-{} {} {}".format(
-            player_1.name, player_1.champion, player_1.kills, player_1.deaths,
-            player_1.assists, player_1.position, player_2.kills,
-            player_2.deaths, player_2.assists, player_2.champion, player_2.name
-            ))
+    print_scoreboard()
