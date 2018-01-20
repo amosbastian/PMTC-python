b6559b3596b4ef87fdee250662879be165ae7ce3
amosbastian <amos_bastian@hotmail.com>
2018-01-20 20:16:55 +0000

    Add flags

--- pmtc_csgo.py
+++ pmtc_csgo.py
@@ -1,25 +1,39 @@
 import requests
+import pycountry
 from bs4 import BeautifulSoup
 from collections import namedtuple
 
 def create_players(table):
     team_data = []
-    Player = namedtuple("Player", 
-        ["name", "k", "a", "d", "kast", "kddiff", "adr", "fkdiff", "rating"])
-
-    for i, row in enumerate(table.select("tr")):
-        if i == 2:
-            print(table.select("tr").find_all("td"))
+    Player = namedtuple("Player", ["name", "k", "a", "d", "kast", "kddiff",
+        "adr", "fkdiff", "rating", "nationality"])
 
     for i, row in enumerate(table.select("tr")):
         if i > 0:
             player_row = row.find_all("td")
+            nationality = player_row[0].find("img").get("alt", "")
             player = [player.text for player in player_row]
+            player.append(nationality)
             team_data.append(Player(*player))
     return team_data
 
 def team_logo(team):
     return "[](#{}-logo)".format(team.lower())
+
+def print_scoreboard(team):
+    for player in team.players:
+        nationality = pycountry.countries.get(name=player.nationality).alpha_2
+        print("|[](#lang-{}) {}|{}|{}|{}|{}|".format(nationality.lower(),
+            player.name, player.k.split()[0], player.a.split()[0],
+            player.d.split()[0], player.rating))
+
+def create_post(team_1, team_2):
+    print("|{} **{}**|**K**|**A**|**D**|**Rating**|".format(
+        team_logo(team_1.name), team_1.name))
+    print("|:--|:--:|:--:|:--:|:--:|")
+    print_scoreboard(team_1)
+    print("|{} **{}**|".format(team_logo(team_2.name), team_2.name))
+    print_scoreboard(team_2)
 
 if __name__ == '__main__':
     response = requests.get("https://www.hltv.org/stats/matches/mapstatsid/59934/g2-vs-cloud9")
@@ -33,9 +47,8 @@ if __name__ == '__main__':
     # print(table_1)
     table_2 = stats_tables[1]
 
-    team = Team(table_1.find_all("th")[0].text, create_players(table_1))
-    # team_2 = Team(table_2.find_all("th")[0].text, create_players(table_2))
+    team_1 = Team(table_1.find_all("th")[0].text, create_players(table_1))
+    team_2 = Team(table_2.find_all("th")[0].text, create_players(table_2))
+    create_post(team_1, team_2)
 
-    # print("|{} **{}**|**K**|**A**|**D**|**Rating**|".format(
-    #     team_logo(team.name), team.name))
-    # print("|:--|:--:|:--:|:--:|:--:|")
+    print()
