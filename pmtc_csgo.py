8ce233272a595e68ef6507b685ed69f02804824a
amosbastian <amos_bastian@hotmail.com>
2018-01-21 12:28:21 +0000

    Now prints full match with working white logo

--- pmtc_csgo.py
+++ pmtc_csgo.py
@@ -38,10 +38,10 @@ def team_logo(team, white=True):
     """
     Takes a team's name and (hopefully) converts it to a team's logo on Reddit.
     """
-    if white:
-        return "[](#{}w-logo)".format(team.lower())
+    if white and teams[team.name]["white"]:
+        return "[](#{}w-logo)".format(team.logo.lower())
     else:
-        return "[](#{}-logo)".format(team.lower())
+        return "[](#{}-logo)".format(team.logo.lower())
 
 def country_converter(country):
     if country == "Russia":
@@ -63,23 +63,36 @@ def print_scoreboard(team):
             player.name, player.k.split()[0], player.a.split()[0],
             player.d.split()[0], player.rating))
 
+def print_overview(team_1, team_2):
+    """
+    Prints the overview of the match.
+    """
+    print("|Team|T|CT|Total|\n|:--|:--:|:--:|:--:|")
+    print("|{}|{}|{}|{}|".format(team_logo(team_1, False),
+        team_1.match.first, team_1.match.second,
+        team_1.match.first + team_1.match.second))
+    print("|{}|{}|{}|{}|".format(team_logo(team_2, False),
+        team_2.match.first, team_2.match.second,
+        team_2.match.first + team_2.match.second))
+    print("\n&nbsp;\n")
+
 def create_post(team_1, team_2):
     """
     Prints the entire scoreboard for both teams.
     """
-    logo = teams[team_1.name]["logo"]
-    initials = teams[team_1.name]["name"]
-    print("|{} **{}**|**K**|**A**|**D**|**Rating**|".format(team_logo(logo),
-        initials))
+    print("\n&nbsp;\n\n###MAP: \n\n&nbsp;\n")
+    print_overview(team_1, team_2)
+    print("|{} **{}**|**K**|**A**|**D**|**Rating**|".format(
+        team_logo(team_1), team_1.initials))
     print("|:--|:--:|:--:|:--:|:--:|")
     print_scoreboard(team_1)
-
-    logo = teams[team_2.name]["logo"]
-    initials = teams[team_2.name]["name"]
-    print("|{} **{}**|".format(team_logo(logo, False), initials))
+    print("|{} **{}**|".format(team_logo(team_2, False), team_2.initials))
     print_scoreboard(team_2)
 
 def count_rounds(half):
+    """
+    Counts how many rounds were won in the half.
+    """
     rounds = 0
     for img in half.find_all("img"):
         if not "emptyHistory.svg" in img["src"]:
@@ -87,6 +100,9 @@ def count_rounds(half):
     return rounds
 
 def team_match(team=1):
+    """
+    Creates a Match object with how many matches the team won in each half.
+    """
     Match = namedtuple("Match", ["first", "second"])
 
     halves = soup.find_all("div", {"class" : "round-history-half"})
@@ -101,33 +117,37 @@ def team_match(team=1):
     Match.second = count_rounds(second_half)
     return Match
 
-if __name__ == '__main__':
-    url = str(sys.argv[1])
-
+def get_response(url):
+    """
+    Gets the response from the given URL with some error checking.
+    """
     if not "www.hltv.org" in url:
-        print("Please enter a URL from www.hltv.org.")
-
+        sys.exit("Please enter a URL from www.hltv.org.")
     try:
         response = requests.get(url)
+        return response
     except Exception as error:
         sys.exit("{}: Please enter a valid URL.".format(repr(error)))
+
+if __name__ == '__main__':
+    url = str(sys.argv[1])
+    response = get_response(url)
 
     with open("csgo.json", "r") as json_data:
         teams = json.load(json_data)
 
     soup = BeautifulSoup(response.text, "lxml")
-    Team = namedtuple("Team", ["name", "players", "match"])
+    Team = namedtuple("Team", ["name", "players", "match", "logo", "initials"])
     
     stats_tables = soup.find_all("table", {"class" : "stats-table"})
 
-    try:
-        table_1 = stats_tables[0]
-        table_2 = stats_tables[1]
+    table_1 = stats_tables[0]
+    table_2 = stats_tables[1]
+    name_1 = table_1.find_all("th")[0].text
+    name_2 = table_2.find_all("th")[0].text
 
-        team_1 = Team(table_1.find_all("th")[0].text,
-            create_players(table_1), team_match(1))
-        team_2 = Team(table_2.find_all("th")[0].text,
-            create_players(table_2), team_match(2))
-        create_post(team_1, team_2)
-    except Exception as error:
-        print(error)
+    team_1 = Team(name_1, create_players(table_1), team_match(1),
+        teams[name_1]["logo"], teams[name_1]["name"])
+    team_2 = Team(name_2, create_players(table_2), team_match(2),
+        teams[name_2]["logo"], teams[name_2]["name"])
+    create_post(team_1, team_2)
