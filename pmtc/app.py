c23fde660d8f3e7ee11dee2af90f92d94539e101
amosbastian <amos_bastian@hotmail.com>
2018-01-24 13:01:26 +0000

    Add form validation and loading spinner

--- pmtc/app.py
+++ pmtc/app.py
@@ -1,28 +1,46 @@
 from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, ValidationError
+
 import sys
 import subprocess
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "fuckoff"
+
+test = True
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+def is_matchhistory(form, field):
+    if not "https://matchhistory" in field.data and len(field.data) < 90:
+        raise ValidationError("Must be a valid match history URL!")
+
+class MatchHistoryForm(FlaskForm):
+    matchhistory = StringField("matchhistory", validators=[InputRequired(),
+        is_matchhistory], render_kw={"placeholder": "Match history"})
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+@app.route("/lol", methods=["GET", "POST"])
+def lol():
+    form = MatchHistoryForm()
+
+    if form.validate_on_submit():
+        url = form.matchhistory.data
+        result = subprocess.check_output([sys.executable, "pmtc_lol.py",
+            url]).decode("ascii")
+        return render_template("lol.html", form=form, result=result)
+    return render_template("lol.html", form=form)
+
 @app.route("/about")
 def about():
     return render_template("about.html")
-
-@app.route("/lol")
-def lol():
-    return render_template("lol.html")
-
-@app.route("/lol", methods=["POST"])
-def lol_pmt(result=None):
-    if request.method == "POST":
-        url = request.form["match-history"]
-        result = subprocess.check_output([sys.executable, "pmtc_lol.py",
-            url]).decode("ascii")
-    return render_template("lol.html", result=result)
 
 @app.route("/csgo")
 def csgo():
