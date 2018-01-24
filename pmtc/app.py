391b5f433842f3ed9aba0ac816b908fd5bd60fd8
amosbastian <amos_bastian@hotmail.com>
2018-01-24 13:37:13 +0000

    Add CSGO page with validation and spinner

--- pmtc/app.py
+++ pmtc/app.py
@@ -15,6 +15,10 @@ test = True
 def index():
     return render_template("index.html")
 
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
 def is_matchhistory(form, field):
     if not "https://matchhistory" in field.data and len(field.data) < 90:
         raise ValidationError("Must be a valid match history URL!")
@@ -22,10 +26,6 @@ def is_matchhistory(form, field):
 class MatchHistoryForm(FlaskForm):
     matchhistory = StringField("matchhistory", validators=[InputRequired(),
         is_matchhistory], render_kw={"placeholder": "Match history"})
-
-@app.route("/test")
-def test():
-    return render_template("test.html")
 
 @app.route("/lol", methods=["GET", "POST"])
 def lol():
@@ -38,13 +38,28 @@ def lol():
         return render_template("lol.html", form=form, result=result)
     return render_template("lol.html", form=form)
 
+def is_hltv(form, field):
+    if (not "https://www.hltv.org/stats/matches/mapstatsid/" in field.data 
+        and len(field.data) < 50):
+        raise ValidationError("Must be a valid HLTV URL!")
+
+class HLTVForm(FlaskForm):
+    hltv = StringField("hltv", validators=[InputRequired(),
+        is_hltv], render_kw={"placeholder": "HLTV"})
+
+@app.route("/csgo", methods=["GET", "POST"])
+def csgo():
+    form = HLTVForm()
+    if form.validate_on_submit():
+        url = form.hltv.data
+        result = subprocess.check_output([sys.executable, "pmtc_csgo.py",
+            url]).decode("ascii")
+        return render_template("csgo.html", form=form, result=result)
+    return render_template("csgo.html", form=form)
+
 @app.route("/about")
 def about():
     return render_template("about.html")
-
-@app.route("/csgo")
-def csgo():
-    return render_template("csgo.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
